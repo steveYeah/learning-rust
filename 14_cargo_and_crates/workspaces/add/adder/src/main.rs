5e78ff73f4188349c083fdc31629966021b97aea
steveYeah <steve.hutchins@gousto.co.uk>
2024-01-29 23:45:06 +0000

    Completed chapter 14

---
+++ 14_cargo_and_crates/workspaces/add/adder/src/main.rs
@@ -0,0 +1,10 @@
+use add_one;
+use add_two;
+
+fn main() {
+    let num = 10;
+    println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+
+    let num = 10;
+    println!("Hello, again! {num} plus two is {}!", add_two::add_two(num));
+}
